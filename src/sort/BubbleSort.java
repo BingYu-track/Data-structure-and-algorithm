87d603379667c22ea13090f0cf0528258d112f90
huxiaowei <525782303@qq.com>
2021-03-13 17:15:27 +0800

    冒泡排序回顾

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -28,52 +28,47 @@ public class BubbleSort {
         }
     }
 
-
-
-
-
-
-
-
-
-
-
     //冒泡排序第一次优化
-    public static void bubbleSortTest2(int[] arr) {
-        for(int i=0;i<arr.length;++i) {
-            for(int j=0;j<arr.length-1-i;++j) { //只需要比较n-1-i次
-                if (arr[j]>arr[j+1]) {
-                    swap(arr, j, j+1);
+    public static void bubbleSortOptimize1(int[] arr){
+        for (int i = 0;i<arr.length-1;i++){
+            //每轮冒泡完成后，都会有一个元素排列到正确的位置，除了第一轮冒泡需要比较n-1次外，
+            //后面的冒泡只需要比较n-1-i次即可,i表示前面已完成的冒泡次数
+            for(int j = 0;j< arr.length-1-i;j++){
+                if (arr[j] > arr[j+1]){
+                    swap(arr,j,j+1);
                 }
             }
         }
     }
 
-    //冒泡排序第二次优化,当所有数字已经是有序时停止排序，例如2,1,3,4,5这里只要冒泡一次即可，第二次如果发现没有元素交换则退出
-    public static void bubbleSort(int[] arr) {
-        if (arr.length<=1) {
+    //冒泡排序第二次优化
+    public static void bubbleSortOptimize2(int[] arr){
+        if (arr.length<=1) { //只有一个元素直接返回
             return;
         }
-        for(int i=0;i<arr.length;++i) {
-            boolean flag = true;
-            for(int j=0;j<arr.length-1-i;++j) { //只需要比较n-1-i次
-                if (arr[j]>arr[j+1]) {
+        for (int i = 0;i<arr.length-1;i++){
+            boolean flag = true; //增加标记变量区分冒泡中是否交换了元素
+            for(int j = 0;j< arr.length-1-i;j++){
+                if (arr[j] > arr[j+1]){
                     flag = false;
-                    swap(arr, j, j+1);
+                    swap(arr,j,j+1);
                 }
             }
-            if (flag) { //只要是true说明前面冒泡没有交换元素，说明元素已经是有序
+            if (flag){ //只要是true说明前面冒泡没有交换元素，说明元素都已经是有序，打断循环
                 break;
             }
         }
     }
 
 
+
+
     /*
     总结：1.冒泡排序是一个原地排序，因为它无需算法辅助空间，空间复杂度为常量
          2.冒泡排序是一个稳定排序，冒泡排序中只有元素交换时才会改变顺序，相同大小的元素不会交换，因此是稳定排序
          3.冒泡排序的时间复杂度分析--,最佳时间复杂度，即已经是有序时，只需进行一次冒泡，n个元素需要执行n-1次交换，因此最佳时间复杂度是O(n);
-            最差时间复杂度，即刚好是逆看序，n个元素需要执行n次冒泡，执行交换的元素次数为n-1+n-2+n-3+....+1=n(n-1)/2;因此最差时间复杂度是0(n^2)
+            最差时间复杂度，即刚好是逆序，n个元素需要执行n-1次冒泡，比较次数随着冒泡的轮数依次递减形成等差数列；
+            执行交换的元素次数为n-1(第一轮冒泡的比较次数)+n-2(第二轮冒泡的比较次数)+n-3(第三轮冒泡的比较次数)+....+1(最后一轮冒泡的比较次数)=n(n-1)/2;因此最差时间复杂度是0(n^2)
 
     */
 
@@ -81,15 +76,16 @@ public class BubbleSort {
         int[] ints = Arrays.copyOf(arr, arr.length);
         System.out.println("冒泡排序开始---------------------");
         long l1 = System.currentTimeMillis();
-        bubbleSort(ints);
+        bubbleSort1(ints);
         long l2 = System.currentTimeMillis();
         long l = l2 - l1;
         System.out.println("冒泡排序结束，花费时间："+ l + "毫秒");
     }
 
     public static void main(String[] args){
-        int[] arr = {9,1,5,8,3,7,4,6,2};
-        bubbleSort1(arr);
+        //int[] arr = {9,1,5,8,3,7,4,6,2};
+        int[] arr = {2,1,3,4,5,6};
+        bubbleSortOptimize2(arr);
         System.out.println(Arrays.toString(arr));
 
     }
