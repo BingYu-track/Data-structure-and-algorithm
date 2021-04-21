2b9b4b7f06b3298fa2e22f0ffce88ab150302e77
huxiaowei <525782303@qq.com>
2021-04-21 23:58:14 +0800

    扑克牌中的顺子

--- src/leetcode/editor/cn/BuKePaiZhongDeShunZiLcof.java
+++ src/leetcode/editor/cn/BuKePaiZhongDeShunZiLcof.java
@@ -29,14 +29,42 @@ package leetcode.editor.cn;
 public class BuKePaiZhongDeShunZiLcof{
     public static void main(String[] args) {
         Solution solution = new BuKePaiZhongDeShunZiLcof().new Solution();
+        int[] arr = {1,2,0,0,5};
         // TO TEST
+        boolean straight = solution.isStraight(arr);
+        System.out.println(straight);
     }
+
+
+    //0表示万能数字，还有除0之外的数字不能重复，还有和排列顺序无关，必须只有5个数
+    //例如：[0,0,8,5,4]可以构成顺子，因为这些抽到的牌可以排成4,5,0,0,8这样
     
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isStraight(int[] nums) {
-
-        return false;
+        if (nums.length != 5) {
+            return false;
+        }
+        boolean[] dup = new boolean[14]; //用来判断是否有重复的数字
+        int max = -1; //最大值
+        int min = 15; //最小值
+        for (int i=0; i<nums.length;i++) {
+            if (nums[i] !=0) {
+                if (dup[nums[i]]) { //第一次进来的数字dup[nums[i]]肯定是false，如果是true说明之前已经遇到过了
+                    return false;
+                }else {
+                    dup[nums[i]] = true;
+                }
+                //注意下面的最小值和最大值要在非0数字的条件下判断
+                if (min > nums[i]) {
+                    min = nums[i];
+                }
+                if (max < nums[i]) {
+                    max = nums[i];
+                }
+            }
+        }
+        return (max - min) < 5; //最大值和最小值相差小于5说明就是顺子
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
