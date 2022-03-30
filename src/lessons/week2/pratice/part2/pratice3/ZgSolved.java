79a08915adad74d3588c9d2ab50b53bc37368e57
huxiaowei <525782303@qq.com>
2022-03-30 11:44:33 +0800

    剑指 Offer 22. 链表中倒数第k个节点

---
+++ src/lessons/week2/pratice/part2/pratice3/ZgSolved.java
@@ -0,0 +1,39 @@
+package lessons.week2.pratice.part2.pratice3;
+
+import lessons.week2.ListNode;
+
+/**
+ * @version 1.0
+ * @Description: 剑指 Offer 22. 链表中倒数第k个节点  争哥解法
+ * @author: bingyu
+ * @date: 2022/3/30
+ */
+public class ZgSolved {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        ListNode node = getKthFromEnd(node1, 6);
+        System.out.println(node);
+    }
+
+    /**
+     * 争哥解法
+     * @param head
+     * @param k
+     * @return
+     */
+    public static ListNode getKthFromEnd(ListNode head, int k) {
+
+        return null;
+    }
+}
