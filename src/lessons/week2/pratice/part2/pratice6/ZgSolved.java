c6222a20a4377ea726491dc6e2579b131aeb82a1
huxiaowei <525782303@qq.com>
2022-04-07 19:48:59 +0800

    环形链表

--- src/lessons/week2/pratice/part2/pratice6/ZgSolved.java
+++ src/lessons/week2/pratice/part2/pratice6/ZgSolved.java
@@ -30,7 +30,15 @@ public class ZgSolved {
      * @return
      */
     public static boolean hasCycle(ListNode head) {
-
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null && slow != fast) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (slow == fast) return true;
         return false;
     }
+
 }
