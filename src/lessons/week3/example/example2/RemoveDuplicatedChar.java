be36481227cfffd66f6f40178b0c323fd9746fc9
huxiaowei <525782303@qq.com>
2022-04-07 22:15:31 +0800

    四道栈例题

---
+++ src/lessons/week3/example/example2/RemoveDuplicatedChar.java
@@ -0,0 +1,16 @@
+package lessons.week3.example.example2;
+
+/**
+ * @version 1.0
+ * @Description: 删除连续重复字符
+ * @author: bingyu
+ * @date: 2022/4/7
+ */
+public class RemoveDuplicatedChar {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/lessons/week3/example/example4/DailyTemperatures.java
@@ -0,0 +1,37 @@
+package lessons.week3.example.example4;
+
+/**
+ * @version 1.0 每日温度
+ * @Description: 给定一个整数数组temperatures，表示每天的温度，返回一个数组answer，其中answer[i]是指在第i天之后，
+ * 才会有更高的温度。如果气温在这之后都不会升高，请在该位置用0来代替。
+ *
+ * 示例 1:
+ * 输入: temperatures = [73,74,75,71,69,72,76,73]
+ * 输出:[1,1,4,2,1,1,0,0]
+ *
+ * 示例 2:
+ * 输入: temperatures = [30,40,50,60]
+ * 输出:[1,1,1,0]
+ *
+ * 示例 3:
+ * 输入: temperatures = [30,60,90]
+ * 输出: [1,1,0]
+ *
+ * 提示：
+ * 1 <=temperatures.length <= 105
+ * 30 <=temperatures[i]<= 100
+ *
+ * @author: bingyu
+ * @date: 2022/4/7
+ */
+public class DailyTemperatures {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+
+        return null;
+    }
+}
