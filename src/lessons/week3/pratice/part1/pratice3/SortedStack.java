0f4b5a446b3feda8a2a1ee51b8ea4ca50c9c42bc
huxiaowei <525782303@qq.com>
2022-04-18 22:12:10 +0800

    栈排序

--- src/lessons/week3/pratice/part1/pratice3/SortedStack.java
+++ src/lessons/week3/pratice/part1/pratice3/SortedStack.java
@@ -31,29 +31,53 @@ public class SortedStack {
 
     private Stack<Integer> stack = new Stack<>();
 
+    private Stack<Integer> tmp = new Stack<>(); //临时栈
+
     private Integer count; //排序栈的元素数目
 
-    //我的思路解法:使用一个临时栈tmp存储比当前元素小的元素
+    //TODO: 注意题目中说的"只能使用一个其他的临时栈存放数据"，意思不是只能使用一个栈，而是额外的临时栈只能使用一个
+    //我的思路解法:需要2个栈，一个栈A和一个临时栈B，栈A先存第一个数据，当后面遇到大的数据就将栈A的数据移动到栈B，然后大的数据放入栈A，再把栈B移动到栈A即可
 
     public SortedStack() {
 
     }
 
     public void push(int val) {
-        if (count == 0) {
-
+        while (!stack.isEmpty() && stack.peek() < val) { //如果栈顶元素小于当前要放入的元素，则将栈顶放入临时栈
+            tmp.push(stack.pop());
+        }
+        //执行到这里说明栈顶元素不小于val了，可以将当前元素放入栈中
+        stack.push(val);
+        //再把临时栈的所有元素移动到栈中
+        while (!tmp.isEmpty()) {
+            stack.push(tmp.pop());
         }
     }
 
     public void pop() {
-
+        if (!stack.isEmpty()) {
+            stack.pop();
+        }
     }
 
     public int peek() {
-        return 0;
+        if (!stack.isEmpty()) {
+          return stack.peek();
+        }
+        return -1;
     }
 
     public boolean isEmpty() {
-        return count == 0;
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        SortedStack sortedStack = new SortedStack();
+        sortedStack.push(1);
+        sortedStack.push(2);
+        sortedStack.push(3);
+        sortedStack.push(4);
+        sortedStack.push(5);
+        System.out.println(sortedStack.peek());
     }
 }
