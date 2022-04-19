6373fbe4a77ecd120ce31084338ad7761f4ee099
huxiaowei <525782303@qq.com>
2022-04-19 15:42:01 +0800

    计算器刷题复习

---
+++ src/lessons/week3/pratice/part2/pratice1/ZgSolved.java
@@ -0,0 +1,21 @@
+package lessons.week3.pratice.part2.pratice1;
+
+/**
+ * @version 1.0 争哥解法
+ * @Description: 计算器
+ * @author: bingyu
+ * @date: 2022/4/19
+ */
+public class ZgSolved {
+
+    public static void main(String[] args) {
+        String expre = "6*3+2*2-20*5/6";
+        int calculate = calculate(expre);
+        System.out.println(calculate);
+    }
+
+    public static int calculate(String s) {
+
+        return 0;
+    }
+}
