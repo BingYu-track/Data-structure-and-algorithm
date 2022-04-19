5c0d78febecc989cef9df48267d03bec3f67e2e3
huxiaowei <525782303@qq.com>
2022-04-19 18:10:11 +0800

    基本计算器 III

---
+++ src/lessons/week3/pratice/part2/pratice2/ZgSolved.java
@@ -0,0 +1,23 @@
+package lessons.week3.pratice.part2.pratice2;
+
+/**
+ * @version 1.0
+ * @Description: 基本计算器 III 争哥解法
+ * @author: bingyu
+ * @date: 2022/4/19
+ */
+public class ZgSolved {
+
+    public static void main(String[] args) {
+        String s = "(2+6*3+5-(3*14/7+2)*5)+3"; //10
+        int calculate = calculate(s);
+        System.out.println(calculate);
+    }
+
+
+    public static int calculate(String s) {
+
+        return 0;
+    }
+
+}
