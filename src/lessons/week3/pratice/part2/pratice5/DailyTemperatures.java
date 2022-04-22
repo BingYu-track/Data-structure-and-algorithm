79d6a5178dc1565d642c8bbde53235f6de60a4e1
huxiaowei <525782303@qq.com>
2022-04-22 14:55:53 +0800

    重刷每日温度

--- src/lessons/week3/pratice/part2/pratice5/DailyTemperatures.java
+++ src/lessons/week3/pratice/part2/pratice5/DailyTemperatures.java
@@ -1,5 +1,8 @@
 package lessons.week3.pratice.part2.pratice5;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * @version 1.0 每日温度
  * @Description: 给定一个整数数组temperatures，表示每天的温度，返回一个数组answer，其中answer[i]是指在第 i 天之后，
@@ -28,12 +31,32 @@ package lessons.week3.pratice.part2.pratice5;
 public class DailyTemperatures {
 
     public static void main(String[] args) {
-
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+        int[] result = dailyTemperatures(temperatures);
+        System.out.println(Arrays.toString(result));
     }
 
-
-    public int[] dailyTemperatures(int[] temperatures) {
-
-        return null;
+    /**
+     *
+     * 核心思路:利用栈存储比后面小的元素，在比较后面元素是不断使用栈的元素下标得到的值与其比较，小于就放在answer里，大于继续向后找
+     * [73,74,75,71,69,72,76,73]
+     * i = 7
+     * stack  [6]
+     * answer [1,1,4,2,1,1]
+     *
+     * 推荐:和争哥的解题思路是一样的
+     */
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int length = temperatures.length;
+        int[] answer = new int[length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0;i < length;i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                Integer peek = stack.peek();
+                answer[peek] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return answer;
     }
 }
