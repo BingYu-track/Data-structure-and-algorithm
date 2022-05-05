85f2d94799c2b760939e9fd07a4cbf623c554363
huxiaowei <525782303@qq.com>
2022-05-05 21:31:44 +0800

    有效的括号--争哥解法

--- src/lessons/week3/pratice/part1/pratice6/ZgSolved.java
+++ src/lessons/week3/pratice/part1/pratice6/ZgSolved.java
@@ -1,8 +1,10 @@
 package lessons.week3.pratice.part1.pratice6;
 
+import java.util.Stack;
+
 /**
  * @version 1.0
- * @Description: 有效的括号
+ * @Description: 有效的括号--争哥解法
  * @author: bingyu
  * @date: 2022/4/18
  */
@@ -14,8 +16,27 @@ public class ZgSolved {
         System.out.println(valid);
     }
 
+    //争哥解法思路和我是一模一样的！
     public static boolean isValid(String s) {
-
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else { //右括号
+                if (stack.empty()) return false;
+                char popC = stack.pop();
+                if (c == ')' && popC != '(') {
+                    return false;
+                }
+                if (c == ']' && popC != '[') {
+                    return false;
+                }
+                if (c == '}' && popC != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
     }
 }
