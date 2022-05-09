345a87101749cc588d099218434d741578c8cea2
huxiaowei <525782303@qq.com>
2022-05-09 17:36:15 +0800

    111

--- src/lessons/week3/pratice/part2/pratice6/ZgSolved.java
+++ src/lessons/week3/pratice/part2/pratice6/ZgSolved.java
@@ -12,7 +12,7 @@ public class ZgSolved {
 
     public static void main(String[] args) {
         int[] height = {2,8,5,5,6,1,7,4,5}; //1-2、 2-3、3-2
-//        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+//      int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
         int trap = trap(height);
         System.out.println(trap);
     }
