c0ab0ff477a4e73cab8c221df417941e84cb7076
huxiaowei <525782303@qq.com>
2022-05-24 18:02:16 +0800

    反转链表--递归解法！

--- src/lessons/week4/pratice/recursion/pratice/pratice5/ReverseList.java
+++ src/lessons/week4/pratice/recursion/pratice/pratice5/ReverseList.java
@@ -28,21 +28,50 @@ public class ReverseList {
         node2.next = node3;
         node3.next = node4;
         node4.next = node5;
+        ListNode listNode = reverseList(node1);
+        System.out.println(listNode);
     }
 
+    /*
+     递归解法:
+     1->2->3->4->5->NULL
+     p.next = newHead
+     递推公式:
+    */
+    public static ListNode v = null; //TODO: 用来存储反转的头节点，这里是重点
     public static ListNode reverseList(ListNode head) {
+        if (head == null) return v;
+        ListNode p = head;
+        reselve(p,p.next);
+        return v;
+    }
 
-        return null;
+    private static ListNode reselve(ListNode p,ListNode next) {
+        if (p == null || p.next == null) {
+            v = p; //在递归的终止条件时，此时遇到的节点就是反转链表的头节点，直接赋给成员变量
+            return p;
+        }
+        ListNode n = reselve(next,next.next);
+        p.next = null;
+        n.next = p;
+        return p;
     }
 
     /*
      普通链表解法:
+     1->2->3->4->5->NULL
      */
     public static ListNode reverseList2(ListNode head) {
-        ListNode vnode = new ListNode(); //哨兵节点
+        ListNode temp = null; //临时节点
         ListNode p = head;
-
-        return null;
+        ListNode newHead = null; //表示上一个节点
+        while (p != null) {
+            temp = p.next; //保存下一个节点，后面要赋值给p做遍历
+            p.next = newHead; //作为反转链表的后继节点
+            newHead = p;
+            p = temp;
+        }
+        return newHead;
     }
 
 }
--- src/lessons/week4/pratice/recursion/pratice/pratice5/ZgSolved.java
+++ src/lessons/week4/pratice/recursion/pratice/pratice5/ZgSolved.java
@@ -11,10 +11,20 @@ import lessons.common.ListNode;
 public class ZgSolved {
 
     public static void main(String[] args) {
-
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        ListNode listNode = reverseList(node1);
+        System.out.println(listNode);
     }
 
-    public ListNode reverseList(ListNode head) {
+    public static ListNode reverseList(ListNode head) {
 
         return null;
     }
