ff8389e07caf107d2c401d175e7c19f0a60c41e0
huxiaowei <525782303@qq.com>
2022-06-13 20:49:10 +0800

    数值的整数次方--争哥解法

--- src/lessons/week4/pratice/recursion/pratice/pratice7/MyPow.java
+++ src/lessons/week4/pratice/recursion/pratice/pratice7/MyPow.java
@@ -2,7 +2,7 @@ package lessons.week4.pratice.recursion.pratice.pratice7;
 
 /**
  * @version 1.0 数值的整数次方
- * @Description: 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，x^n）。不得使用库函数，同时不需要考虑大数问题。
+ * @Description: 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，x^n）。不得使用库函数，同时不需要考虑大数问题(指不考虑溢出)。
  * 示例 1：
  * 输入：x = 2.00000, n = 10
  * 输出：1024.00000
@@ -36,15 +36,15 @@ public class MyPow {
       x^n = x*x
     */
     public static int time = 0; //用来保存执行的次数
-    public static double myPow(double x, int n) {
-        if (n == 0 || x == 1) return 1;
-        double result = x;
-        if (n > 0) {
+    public static double myPow(double x, int n) { //n表示n次冥
+        if (n == 0 || x == 1) return 1; //次幂是0或者数字是1，直接返回1
+        double result = x; //存储计算结果
+        if (n > 0) { //如果次幂大于0，从1开始计数，否则从-1开始计数
             time = 1;
         }else {
             time = -1;
         }
-        result = pow(x, n, result);
+        result = pow(x, n, result); //开始计算次方
         while (n>0 && (time>n || time<=0)) {
             result = result / x;
             time--;
@@ -53,12 +53,13 @@ public class MyPow {
             result = result / x;
             time++;
         }
-        if (n < 0) result = 1 / result;
+        if (n < 0) result = 1 / result; //如果n次幂是负数，那么就是1/result
         return result;
     }
 
 
     private static double pow(double x, int n, double result) {
+        //这里n>0而且time<=0
         if ((n>0 && (time>=n || time<=0)) || (n<0 && (time<=n || time>=0))) { //终止条件:一旦执行的次幂超过n就终止，后面需要进行返回处理
             return result;
         }
@@ -73,6 +74,7 @@ public class MyPow {
      迭代法: 注意n取int范围的最小值，除的时候就会导致超出int边界范围;另外因为数据规模是n次幂，因此该方法
      的时间消耗也是指数级的，我们需要想办法降低执行的次数，否则会超时,其难点再与处理越界问题！
      TODO: 注意 -1^(-10) = 1/(-1^10),-1的负10次幂就是-1除以自己10次，此时幂是偶数，结果就成了正数
+           核心思路是 通过自己乘以自己快速逼近结果值
     */
     public static double myPow2(double x, int n) {
         if (n == 0 || x == 1) return 1;
@@ -89,13 +91,13 @@ public class MyPow {
                 result = result / x;
                 i++;
             }
-        }else {
-            int i = 1;
-            for (;i<time && count<31;i+=i) { //i加后会越界变为负数
+        }else { //正次幂
+            int i = 1;//表示当前执行的次数
+            for (;i<time && count<31;i+=i) { //i加加后，后面会越界变为负数
                 result = result * result;
                 count++;
             }
-            while (i>time || i<0) { //i<0说明越界，超过了int的最大范围值
+            while (i>time || i<0) { //i<0说明越界，超过了int的最大范围值，赶快除回来并减掉多余的幂次数
                 result = result / x;
                 i--;
             }
--- src/lessons/week4/pratice/recursion/pratice/pratice7/ZgSolved.java
+++ src/lessons/week4/pratice/recursion/pratice/pratice7/ZgSolved.java
@@ -2,18 +2,41 @@ package lessons.week4.pratice.recursion.pratice.pratice7;
 
 /**
  * @version 1.0
- * @Description: 数值的整数次方--争哥解法
+ * @Description: 数值的整数次方--争哥解法 (需要复习)
  * @author: bingyu
  * @date: 2022/5/25
  */
 public class ZgSolved {
 
     public static void main(String[] args) {
-
+        double result = myPow(-1,2147483647);
+        System.out.println(result);
     }
 
+    /*
+     TODO 争哥解法思路: 推荐!
+          递推公式： n是偶数的情况下--f(x,n) = f(x,n/2) * f(x,n/2)
+                   n是奇数的情况下--f(x,n) = f(x,n/2) * f(x,n/2) * x
+     */
     public static double myPow(double x, int n) {
+        if (n >= 0) {
+            return rPow(x, n);
+        }else { //n次幂小于0  -2^31~2^31-1  = -2147483648 ~ 2147483647
+        //return 1 / rPow(x, -1*n); 注意不能这样写，因为当x=2,n=-2147483648 ，再转换成正数就rPow(x,2147483648),n就越界了,
+       // 因此我们把rPow(x, -1*n) 凑成 rPow(x, -1*(n+1))*x  ，x的-1*(n+1))次幂再乘以x,同样等于x的-1*n次幂，但是却会避免越界
+       // 此时n=-2147483648时就成了rPow(x,-1*-21474836477)*x = rPow(x,21474836477) * x，这样就不会越界了
+            // x=2.00000 n=-2147483648
+            return 1/(rPow(x, -1*(n+1))*x);
+        }
+    }
 
-        return 0d;
+    public static double rPow(double x, int n) {
+        if (n == 0) return 1; //终止条件，不断进行n/2直到n==0
+        double halfPow = rPow(x, n/2);
+        if (n % 2 == 1) { //如果n次幂是奇数
+            return halfPow * halfPow * x;
+        } else { //n次幂是偶数
+            return halfPow * halfPow;
+        }
     }
 }
