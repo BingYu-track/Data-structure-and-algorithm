5bf22fa577161d20ce525392e9ca4c8bd93a05bf
huxiaowei <525782303@qq.com>
2022-07-06 19:55:15 +0800

    有效的字母异位词--争哥解法

--- src/lessons/week4/pratice/sorted/pratice/pratice2/ZgSolved.java
+++ src/lessons/week4/pratice/sorted/pratice/pratice2/ZgSolved.java
@@ -1,5 +1,7 @@
 package lessons.week4.pratice.sorted.pratice.pratice2;
 
+import java.util.Arrays;
+
 /**
  * @version 1.0 有效的字母异位词
  * @Description: 争哥解法
@@ -17,11 +19,20 @@ public class ZgSolved {
 
 
     /*
-
+    争哥思路: 直接将两个字符串安装字母顺序排序，然后遍历比较
     */
     public static boolean isAnagram(String s, String t) {
-
-        return false;
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        for (int i=0;i< str1.length;i++) {
+            if (str1[i] != str2[i]) return false;
+        }
+        return true;
     }
 
 }
