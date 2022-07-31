be70ecd896679e86446eede0a014d177f5a594ee
huxiaowei <525782303@qq.com>
2022-07-31 18:51:16 +0800

    例题

---
+++ src/lessons/week5/example/binarysearch/example5/PeakIndexInMountainArray.java
@@ -0,0 +1,22 @@
+package lessons.week5.example.binarysearch.example5;
+
+/**
+ * @version 1.0
+ * @Description: 查找峰值--对应leetcode的"山脉数组的峰顶索引"
+ * @author: bingyu
+ * @date: 2022/7/28
+ */
+public class PeakIndexInMountainArray {
+
+    public static void main(String[] args) {
+        int[] arr = {24,69,100,99,79,78,67,36,26,19};
+        int target = 2;
+        int i = peakIndexInMountainArray(arr);
+        System.out.println(i);
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+
+        return 0;
+    }
+}
