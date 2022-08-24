28af9d8409cc45de311e6c252b5015bead739f39
huxiaowei <525782303@qq.com>
2022-08-24 14:12:14 +0800

    二叉树公共类

---
+++ src/lessons/common/TreeNode.java
@@ -0,0 +1,25 @@
+package lessons.common;
+
+/**
+ * @version 1.0
+ * @Description:
+ * @author: bingyu
+ * @date: 2022/8/24
+ */
+public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     TreeNode() {
+
+     }
+    public TreeNode(int val) {
+         this.val = val;
+     }
+     public TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+
+ }
