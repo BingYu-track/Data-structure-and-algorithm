63220bcdc063d3b8a682a3059ff6f6c80868a27a
huxiaowei <525782303@qq.com>
2022-08-28 21:50:54 +0800

    111

---
+++ src/lessons/week6/example/btree/example2/InorderTraversal.java
@@ -0,0 +1,55 @@
+package lessons.week6.example.btree.example2;
+
+import lessons.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @version 1.0
+ * @Description: 二叉树的中序遍历
+ * @author: bingyu
+ * @date: 2022/8/24
+ */
+public class InorderTraversal {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        List<Integer> list = inorderTraversal(root);
+        System.out.println(list);
+    }
+
+
+    /*   左根右
+            1    [1,2,3,4,5,6,7]  -->预期[4,2,5,1,6,3,7]
+          /   \
+         2     3
+        / \   /  \
+       4   5 6    7
+    */
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root,list);
+        return list;
+    }
+
+    private static void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
+    }
+
+}
