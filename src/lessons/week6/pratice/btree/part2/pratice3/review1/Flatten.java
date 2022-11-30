937e98fd4e609c045ba09fb66b238d6d5a65d3b5
huxiaowei <525782303@qq.com>
2022-11-30 10:04:39 +0800

    二叉树展开为链表 -- 复习

--- src/lessons/week6/pratice/btree/part2/pratice3/review1/Flatten.java
+++ src/lessons/week6/pratice/btree/part2/pratice3/review1/Flatten.java
@@ -1,9 +1,9 @@
-package lessons.week6.pratice.btree.part2.pratice3;
+package lessons.week6.pratice.btree.part2.pratice3.review1;
 
 import lessons.common.TreeNode;
 
-/** TODO: 需要多次练习
- * @version 1.0 二叉树展开为链表
+/**
+ * @version 1.0 二叉树展开为链表 -- 复习
  * @Description: 给你二叉树的根结点 root ，请你将它展开为一个单链表：
  * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
  * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
@@ -30,7 +30,7 @@ import lessons.common.TreeNode;
  * -100 <= Node.val <= 100
  *
  * @author: bingyu
- * @date: 2022/9/13
+ * @date: 2022/11/28
  */
 public class Flatten {
 
@@ -52,41 +52,27 @@ public class Flatten {
         System.out.println(root);
     }
 
-    /*
-     争哥思路解法: 在前序遍历期间进行展开，使用虚拟头节点作为起始节点，然后一个tail随时指向末尾，tail不断指向当前root节点
-     TODO： 需要多多练习
-     执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
-     内存消耗：41.3 MB, 在所有 Java 提交中击败了13.14%的用户
-    */
     public static void flatten(TreeNode root) {
-//        preOrder(root);
-        inOrder(root);
+        preOrder(root);
     }
-    private static TreeNode dummyHead = new TreeNode(); //虚拟头节点
-    private static TreeNode tail = dummyHead; //记录链表尾节点
 
-    //在前序遍历的同时进行展开
+    private static TreeNode dummyHead = new TreeNode();//虚拟头节点
+    private static TreeNode tail = dummyHead; //保存链表的尾节点;因为需要时刻获取tail，因此要作为共享的成员变量
+
+    /*
+     这里重点是如何正确设置链表尾节点，使tail始终保持在末尾
+     TODO: 由于该题展开的链表顺序是和前序遍历顺序是一致的，因此需要我们借助前序遍历来解决
+    */
     private static void preOrder(TreeNode root) {
         if (root == null) return;
-        TreeNode left = root.left; //
-        TreeNode right = root.right; //这个我怎么存
-        tail.right = root; //将当前root作为下一个节点
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        tail.right = root; //尾节点指向下一个尾节点
         tail = root;
         root.left = null;
         preOrder(left);
         preOrder(right);
     }
 
-    //TODO: 中序遍历 3 2 4 1 5 6和规定的1 2 3 4 5 6不一致，因此不能使用中序遍历来解决该题
-    private static void inOrder(TreeNode root) {
-        if (root == null) return;
-        TreeNode left = root.left; //
-        TreeNode right = root.right;
-        inOrder(left);
-        tail.right = root;
-        tail = root;
-        root.left = null;
-        inOrder(right);
-    }
 
 }
