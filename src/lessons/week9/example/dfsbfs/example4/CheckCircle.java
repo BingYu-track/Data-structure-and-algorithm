9b0fca9b8ee8248dd04dbbd0cf1915b3b2a6506c
huxiaowei <525782303@qq.com>
2023-03-17 00:15:53 +0800

    有向图，非联通图理解和其相关代码实现

--- src/lessons/week9/example/dfsbfs/example4/CheckCircle.java
+++ src/lessons/week9/example/dfsbfs/example4/CheckCircle.java
@@ -1,5 +1,11 @@
 package lessons.week9.example.dfsbfs.example4;
 
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+import lessons.week9.graph.DiGraph;
+import lessons.week9.graph.UndiGraph;
+
+import java.util.List;
+
 /**
  * @version 1.0
  * @Description: 题型5，检测环
@@ -15,5 +21,97 @@ public class CheckCircle {
      解决思路: 首先根据给出的两两关系构建出一个邻接表，用hashMap记录上面两个关系，遇到重复的就加1，例如
      [A,B]是A->B，那么A在hashMap中的value是0，B是1，后面[B,C]B->C,B仍然是1，C的value是1；到[C,A]后就是
      A的value变成1了，那么A,B,C的入度都不为0，那么肯定是有环的！
+
+     TODO： 1.无向图判断是否有环
+             方法1（数学方法）： 图的顶点数为n，边数为m，若n>=m+1，则无环;否则有环。
+             方法2：使用并查集进行判断。
+             方法3：DFS。使用visited数组辅助判断是否访问过。
+
+
+     TODO： 2.有向图判断是否有环
+             方法一：每次从有向图中取出一个入度为 0 的节点删除，同时删除该节点及所有以他为起点的边，若最终图为空则证明无环，最终非空则证明有环
+            为什么呢？
+             我们来单独考虑一个单环，那么环中每一个点都是入度为 1，出度也为 1，即不可能入度为 0。按上面删环的描述过程，
+             如果环存在，这个环中的的每个点都无法有机会变成入度为 0 的点，因此就证明了环的存在。
+            方法二: DFS。使用一个color数组表示节点类型，color[i]=0表示该节点未被访问，color[i]=1表示该节点正在当前访问的路径中或该节点
+            存在于环路中，color[i]=2表示该节点是安全不存在于环中的。
+
+            用一个布尔数组记录DFS遍历的路径。每处理一个节点时，将其加入到路径数组，当所有子树处理完了的时候，再清空路径数组。
+     */
+
+    public static void main(String[] args) {
+        UndiGraph undiGraph = new UndiGraph(5); //创建无向图
+        DiGraph graph = new DiGraph(7); //创建有向图
+        graph.addEdge('g'-'a','b'-'a'); //添加g-->b的边
+        graph.addEdge('g'-'a','c'-'a'); //添加g-->c的边
+        graph.addEdge('b'-'a','c'-'a'); //添加b-->c的边
+        graph.addEdge('b'-'a','d'-'a'); //添加b-->d的边
+        graph.addEdge('b'-'a','e'-'a'); //添加b-->e的边
+        graph.addEdge('d'-'a','f'-'a'); //添加d-->f的边
+        graph.addEdge('e'-'a','f'-'a'); //添加e-->f的边
+        graph.addEdge('f'-'a','a'-'a'); //添加f-->g的边
+        graph.addEdge('a'-'a','e'-'a'); //添加a-->e的边
+        boolean cicyle = graph.checkCicyle(); //检测环,注意这种方法检测环会修改图的结构，不推荐这种方法
+        System.out.println("有向连通图检测环(有环): "+cicyle);
+        boolean cicyleByDfs = graph.checkCicyleByDfs(); //通过DFS来检测环
+        System.out.println("有向连通图DFS检测环: "+cicyleByDfs);
+
+        /*
+                   有向连通图:
+                           s顶点
+                            (g)
+                          /     \
+                         |       |
+                         V       V
+                        (b)---->(c)
+                       /  \
+                      V    V
+                     (d)  (e)<--(a)  t顶点
+                      \    |    ↑
+                       \   |   /
+                        V  V  /
+                        (  f  )
+        */
+        System.out.println();
+
+        DiGraph graph3 = new DiGraph(10); //创建一个有向非连通图
+        graph3.addEdge('a'-'a','b'-'a'); //添加a-->b的边
+        graph3.addEdge('a'-'a','e'-'a'); //添加a-->e的边
+        graph3.addEdge('b'-'a','c'-'a'); //添加b-->c的边
+        graph3.addEdge('b'-'a','d'-'a'); //添加b-->d的边
+        graph3.addEdge('d'-'a','e'-'a'); //添加d-->e的边
+        graph3.addEdge('g'-'a','h'-'a'); //添加g-->h的边
+        graph3.addEdge('h'-'a','i'-'a'); //添加h-->i的边
+        graph3.addEdge('h'-'a','j'-'a'); //添加h-->j的边
+        graph3.addEdge('i'-'a','g'-'a'); //添加i-->g的边
+        boolean checkCicyle = graph3.checkCicyle();
+        System.out.println("有向非连通图检测环: " + checkCicyle);
+        boolean checkCicyleByDfs = graph3.checkCicyleByDfs();
+        System.out.println("有向非连通图DFS检测环: " + checkCicyleByDfs);
+        if (!checkCicyle) {
+            List<String> toposortByKahn3 = graph3.topoSortByKahn();
+            System.out.println("有向非连通图Kahn算法拓扑排序: " + toposortByKahn3); //[a, f, g, b, h, c, d, i, j, e]
+        }
+        if (!checkCicyleByDfs) {
+            List<String> topoSortByDFS = graph3.topoSortByDFS();
+            System.out.println("有向非连通图DFS算法拓扑排序: " + topoSortByDFS); //[g, h, j, i, f, a, b, d, e, c]
+        }
+
+
+
+    }
+
+    /*
+                    有向非连通图:
+                 c<---b<-----a
+                      |      |
+                      |      |
+                      V      V
+                      d----> e
+
+                      g--->h--->j
+                           |
+                           v
+                           i
      */
 }
--- src/lessons/week9/graph/DiGraph.java
+++ src/lessons/week9/graph/DiGraph.java
@@ -3,7 +3,6 @@ package lessons.week9.graph;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * @version 1.0
@@ -26,9 +25,11 @@ public class DiGraph {
         }
     }
 
+    //添加顶点s到t的边
     public void addEdge(int s,int t) {
         adj[s].add(t);
     }
+
 
     //TODO 1.查找点s到点t是否有路径
     public boolean foundPath(char s,char t) {
@@ -111,7 +112,10 @@ public class DiGraph {
         //取入度为0的元素
         while (!zeroDegree.isEmpty()) {
             int poll = zeroDegree.poll(); //入度为0的顶点
-            sorted.add(String.valueOf((char)(poll + 'a'))); //将入度为0的顶点放入序列中
+            //将入度为0的顶点放入集合中
+            //TODO: 如果所有顶点的入度都能成为0，说明这个图没有环，有环的话，不断去掉关联顶点，入度不可能全是0，
+            // 另外如果是非连通图的话，没有环，每个子图肯定也会有入度为0的点，因此该算法也适合非连通图
+            sorted.add(String.valueOf((char)(poll + 'a')));
             for (int i = 0;i < adj[poll].size();i++) {
                 inDegrees[adj[poll].get(i)]--; //依赖与poll顶点的点入度都需要减1
                 if (inDegrees[adj[poll].get(i)] == 0) {
@@ -124,16 +128,23 @@ public class DiGraph {
 
     /*
     TODO: 这个比较难理解，需要重点掌握!
-      b.DFS算法
-        使用DFS算法有个前提，就是创建图时需要按照依赖关系来创建，例如a-->b 一般创建a到b的边是graph.addEdge(a,b)，
-        如果用DFS，我们就要用b-->a来表示，即graph.addEdge(b,a);
-       解题思路:  正常我们走的路径排序是[a, b, c, d, e, f, g]，如果按照依赖关系，构造的图按照之前的算法得到的就是[g,f,e,d,c,b,a]，
-        我们通过DFS递归可以将其序列顺序进行反转。
+      b. DFS算法 遍历有向图实际上就是进行拓扑排序！
+        如果我们对每个节点进行DFS会发现(假设无环),会一直向下执行直到后面没有节点才会返回，我们在回溯时把顶点放入集合时，会发现，其点
+        在集合的的顺序和路径的顺序刚好是相反的，因此我们只需要每次在回溯放入时addFirst,放到集合的头部，这样我们集合中存储的点的顺序就是
+        拓扑顺序了!
+
+       问题: 上面是遍历的连通图，那如果我们遍历的是非连通图呢?
+        答: 对于非连通图，DFS遍历过程执行之后，图中一定还有顶点未被访问，需要从图中另选一个未被访问的顶点作为起始点，再重复DFS，直到图中所有顶点
+        均被访问过为止!
     */
     public List<String> topoSortByDFS() {
         boolean[] visited = new boolean[v];
-        List<String> sorted = new ArrayList<>();
-        for (int i = 0;i<v;i++) { //遍历多余顶点
+        LinkedList<String> sorted = new LinkedList<>();
+        visited[0] = true;
+        dfs(0,sorted,visited);
+        //TODO: 遍历所有未访问过的顶点，之所以这样做，就是为了考虑非连通图，这样的话，子图中dfs不到的点，也会被作为起始点进行DFS,
+        // 如果只是连通图，我们只需要dfs(i,sorted,visited);这一个代码即可!
+        for (int i = 0;i < v;i++) {
             if(!visited[i]) {
                 visited[i] = true;
                 dfs(i,sorted,visited);
@@ -142,21 +153,24 @@ public class DiGraph {
         return sorted;
     }
 
-    /* 先找入度为0，且
-      先把当前顶点k所依赖的点全部放入集合，最后再将当前顶点放入集合
+    /**
+     * 先把当前顶点k所依赖的点全部放入集合，最后再将当前顶点放入集合
+     * @param k 初始顶点
+     * @param sorted
+     * @param visited 标记是否被访问过
      */
-    private void dfs(int k,List<String> sorted,boolean[] visited) {
-        for (int i = 0;i < adj[k].size();i++) {
-            int w = adj[k].get(i);  //依赖的顶点
+    private void dfs(int k,LinkedList<String> sorted,boolean[] visited) {
+        for (int i = 0;i < adj[k].size();i++) { //遍历k下面的点
+            int w = adj[k].get(i);
+            //System.out.print((char)(w + 'a')+","); TODO 不能这样，因为这样就成了横向遍历了，而不是沿着一条方向的路径
             if (!visited[w]) { //如果当前依赖的w点未访问过，就继续遍历w所依赖的点
                 visited[w] = true;
                 dfs(w,sorted,visited); //不断深度遍历k点所依赖的所有顶点
             }
         }
-        //执行到这里说明顶点k所依赖的点已全部处理完了
-        sorted.add((char)(k + 'a')+"");
+        //执行到这里说明k下面的点已全部处理完毕，将当前顶点放入集合的头部
+        sorted.addFirst((char)(k + 'a')+"");
     }
-
 
 
     /*
@@ -196,8 +210,87 @@ public class DiGraph {
                            \      /
                             V    V
                              ( g )
-           TODO DFS输出:[g, b, d, e, f, a, c]，注意这也是正确的拓扑排序，因为c只依赖g和b。因此c排到b后面的任意一个位置都可以！
+           TODO:  Kahn算法输出:[a, c, f, d, e, b, g]；DFS拓扑排序输出: [c, a, f, e, d, b, g]
+            注意2个都是正确的拓扑排序，因为a和c都没有任何依赖，因此任何一个位置都可以，e,d只依赖f，因此e和d只要
+            在f的后面任意一个位置都可以！
     */
+
+
+  /* TODO： c.检测图中是否有环
+      方法1: 统计出有向图中入度为0的节点并放入队列，每次遍历这些入度为0的节点时，将其关联的节点入度减1，如果有新的入度为0的节点，就放入
+      队列，我们用zeroDegreeCount记录队列遍历中的所有入度为0的节点个数，遍历完后，如果这个节点个数等于这个图节点总个数说明没有环，否则有环;
+      因为我们每次从有向图中取出一个入度为 0 的节点，同时删除该节点及所有以他为起点的边，若最终图为空则证明无环，最终非空则证明有环，
+      可以这里理解: 一个单环，那么环中每一个点都是入度为1，出度也为 1，即不可能入度为 0。按上面取节点的描述过程，如果环存在，这个环中的的每个点都无法
+      有机会变成入度为0的点，即zeroDegreeCount小于总节点个数，因此就证明了环的存在。
+     */
+    public boolean checkCicyle() {
+        //先统计出有向图中每个节点的入度
+        int[] inDegrees = new int[v]; //用来存储每个顶点的入度
+        LinkedList<Integer> zeroDegree = new LinkedList<>(); //用来统计入度为0的顶点
+        for (int i = 0;i < v;i++) {
+            for (int j = 0;j < adj[i].size();j++) { //adj[i].get(j)点依赖于i点，因此其入度加1
+                inDegrees[adj[i].get(j)]++;
+            }
+        }
+        for (int i = 0;i<inDegrees.length;i++) {
+            if (inDegrees[i]==0) {
+                zeroDegree.add(i);
+            }
+        }
+        int zeroDegreeCount = 0; //TODO: 这里是重点，记录入度为0的节点个数
+        while (!zeroDegree.isEmpty()) {
+            int p = zeroDegree.poll();
+            zeroDegreeCount++;
+            for (int i = 0;i<adj[p].size();i++) { //删除和p顶点相连的边，并且其关联的顶点入度减1
+                inDegrees[adj[p].get(i)]--;
+                if (inDegrees[adj[p].get(i)] == 0) zeroDegree.add(adj[p].get(i));
+            }
+        }
+        return !(zeroDegreeCount == v); //入度为0的节点个数等于总节点个数，说明无环
+    }
+
+    private boolean isCicyle = false;
+
+    /*
+     思路2: 使用dfs检测环，先从入度为0的节点开始dfs，每个个节点在初始状态为白色，访问该节点时染成红色，并在退出时染成黑色。如果DFS遍历中发现了
+       红色节点，则说明有环。先选取任意一个未访问过的点开始遍历
+   TODO: 我之前写的方法不适用于非连通图，因为我只找一个入度为0的点，然后进行dfs遍历，如果是非连通图的话，会有多个入度为0的点，
+    这样可能就错过遍历另一个子图了，如果刚好这个子图里有环，那这就会判断错误;还有一种可能就是子图里形成了一个环，子图里所有的顶点入度
+    都不为0.我这种dfs根本就无法遍历到这个子图上，也会出错! 所以在使用dfs时需要先遍历多余节点，对每个未访问的节点进行DFS，然后再
+    */
+    public boolean checkCicyleByDfs() {
+        int[] color = new int[v]; //0表示未访问 1表示节点正在被访问的路径中  2表示节点已经退出了当前路径 (如果后续遍历发现了1，说明有环)
+        LinkedList<Integer> list = new LinkedList<>(); //用来存储回溯时的节点
+        for (int i = 0;i < v;i++) {
+            if (color[i] == 0) { //如果没访问该节点，就从这个未访问的节点开始进行DFS
+                color[i] = 1; //标记为已访问，并在当前路径中
+                dfs(i,color,list);
+            }
+        }
+        return isCicyle;
+    }
+
+    /**
+     *
+     * @param k 起始点
+     * @param color 用来记录每个点的状态
+     * @param list 存储的拓扑排序集合
+     */
+    private void dfs(int k, int[] color, LinkedList<Integer> list) {
+        for (int i = 0;i<adj[k].size();i++) { //向下遍历k连接的所有顶点
+            int w = adj[k].get(i);
+            if (color[w] == 0) {
+                color[w] = 1;
+                dfs(w,color,list);
+            }else if (color[w] == 1) { //如果遍历过程中遇到前面路径中访问过的节点，说明存在环
+                isCicyle = true;
+                return;
+            }
+        }
+        //TODO: 执行到这里说明k下面的节点已经全部执行完,k节点需要返回到上一层了，撤销
+        list.addFirst(k);
+        color[k] = 2;
+    }
 
 
     public static void main(String[] args) {
@@ -220,8 +313,11 @@ public class DiGraph {
 
         List<String> toposortByKahn = graph.topoSortByKahn();
         System.out.println("原图Kahn算法拓扑排序: " + toposortByKahn); //[g, b, c, d, e, f, a]
+        List<String> sortByDFS = graph.topoSortByDFS();
+        System.out.println("原图DFS算法拓扑排序: " + toposortByKahn); //[g, b, c, d, e, f, a]
+        System.out.println();
 
-        DiGraph graph2 = new DiGraph(7);
+        DiGraph graph2 = new DiGraph(7); //将上面的有向图变成依赖关系图
         graph2.addEdge('b'-'a','g'-'a'); //添加b-->a的边
         graph2.addEdge('c'-'a','g'-'a'); //添加c-->a的边
         graph2.addEdge('c'-'a','b'-'a'); //添加c-->b的边
@@ -233,7 +329,36 @@ public class DiGraph {
         List<String> toposortByKahn2 = graph2.topoSortByKahn();
         System.out.println("依赖图Kahn算法拓扑排序: " + toposortByKahn2); //[a, c, f, d, e, b, g]
         List<String> topoSortByDFS = graph2.topoSortByDFS();
-        System.out.println("依赖图DFS拓扑排序: " + topoSortByDFS); //[g, b, d, e, f, a, c]
-        //
+        System.out.println("依赖图DFS拓扑排序: " + topoSortByDFS); //[c, a, f, e, d, b, g]
+
+
+
     }
+
+/*
+        TODO: 任意2个顶点之间都存在路径，那么称为连通图（注意是任意2顶点）。上面的图每个顶点都有路径，因此是连通图。而下面这张图中，
+         顶点8和顶点2之间就不存在路径，因此下图不是一个连通图，当然该图中还有很多顶点之间不存在路径。虽然不是一个连通图，但它有多个
+         连通子图：0,1,2顶点构成一个连通子图，0,1,2,3,4顶点构成的子图是连通图，6,7,8,9顶点构成的子图也是连通图，当然还有很多子图。
+         我们把一个图的最大连通子图称为它的连通分量。0,1,2,3,4顶点构成的子图就是该图的最大连通子图，也就是连通分量！
+         连通分量有如下特点：
+            1)是子图；
+            2)子图是连通的；
+            3)子图含有最大顶点数。
+            注意：“最大连通子图”指的是无法再扩展了，不能包含更多顶点和边的子图。0,1,2,3,4顶点构成的子图已经无法再扩展了。
+            显然，对于连通图来说，它的最大连通子图就是其本身，连通分量也是其本身
+
+
+                        2<----1<-----0
+                              |      |
+                              |      |
+                              V      V
+                              3----> 4
+
+                         6---->7--->9
+                               |
+                               v
+                               8
+                 注意:这2个子图构成一个非连通图
+
+    */
 }
