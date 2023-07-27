897b458e93fd8b471beb940ca133964c2cb06604
huxiaowei <525782303@qq.com>
2023-07-27 15:59:30 +0800

    双指针和滑动窗口总结

---
+++ src/lessons/week12/doublepointer/DoublePointer.java
@@ -0,0 +1,12 @@
+package lessons.week12.doublepointer;
+
+/**
+ * @version 1.0
+ * @Description: 双指针
+ * @author: bingyu
+ * @date: 2023/7/12
+ */
+public class DoublePointer {
+
+
+}
---
+++ src/lessons/week12/slidingwindow/SlidingWindow.java
@@ -0,0 +1,16 @@
+package lessons.week12.slidingwindow;
+
+/**
+ * @version 1.0
+ * @Description: 滑动窗口
+ * @author: bingyu
+ * @date: 2023/7/12
+ */
+public class SlidingWindow {
+
+
+    /*
+     滑动窗口一般用来处理连续的........
+     如果看到连续的字符串，连续的数字等等，可以尝试使用滑动窗口来解决。
+    */
+}
