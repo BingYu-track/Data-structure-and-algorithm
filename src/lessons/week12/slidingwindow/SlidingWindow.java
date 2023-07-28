d444aa220eb088e41bb88bedb9184e02d4c355da
huxiaowei <525782303@qq.com>
2023-07-28 15:48:25 +0800

    滑动窗口总结

--- src/lessons/week12/slidingwindow/SlidingWindow.java
+++ src/lessons/week12/slidingwindow/SlidingWindow.java
@@ -11,6 +11,7 @@ public class SlidingWindow {
 
     /*
      滑动窗口一般用来处理连续的........
-     如果看到连续的字符串，连续的数字等等，可以尝试使用滑动窗口来解决。
+     如果看到连续的字符串，连续的数字等等，可以尝试使用滑动窗口来解决,但是要注意如果有负数可能就不行了，因为这样就不知道什么时候q++，什么时候p++，详情可见
+      lessons.week12.psstatistics.pratice.pratice1.MaxSubArray。
     */
 }
