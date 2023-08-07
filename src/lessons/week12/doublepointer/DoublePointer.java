1ad68da808aea587f3519121dc15c32f15f6bfbb
huxiaowei <525782303@qq.com>
2023-08-07 15:09:22 +0800

    双指针总结

--- src/lessons/week12/doublepointer/DoublePointer.java
+++ src/lessons/week12/doublepointer/DoublePointer.java
@@ -2,11 +2,14 @@ package lessons.week12.doublepointer;
 
 /**
  * @version 1.0
- * @Description: 双指针
+ * @Description: 双指针总结
  * @author: bingyu
  * @date: 2023/7/12
  */
 public class DoublePointer {
 
-
+    /*
+      我的对双指针的昨天总结是: 双指针不止包含首位向中间移动的这种，还包含快慢指针，同样属于双指针；向中间移动的这种一般来说
+      都是需要先进行排序，才能再进行移动的！
+     */
 }
